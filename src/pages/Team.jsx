fb2f1226072c5ccf1785a25ab9ce8afd69d8154e
Seun <124383448+seun-ipaye@users.noreply.github.com>
2026-06-03 11:42:07 -0400

    More roles

--- src/pages/Team.jsx
+++ src/pages/Team.jsx
@@ -3,10 +3,16 @@ import Footer from "../components/Footer";
 
 function Team() {
   const members = [
-    { role: "President", name: "Name here" },
-    { role: "Vice President", name: "Name here" },
-    { role: "Events Lead", name: "Name here" },
-    { role: "Outreach Lead", name: "Name here" },
+    { role: "Co-President", name: "Ewa Olakunle" },
+    { role: "Co-President", name: "Seun Samuel-Ipaye" },
+    { role: "Vice President", name: "Sura Gaafar" },
+    { role: "Events Lead", name: "Adedeji Adeboyejo" },
+    { role: "Head of Technology", name: "Manzi Munyambo" },
+    { role: "Treasurer", name: "Samuel Obawole" },
+    { role: "Outreach Lead", name: "Nifemi Koleosho" },
+    { role: "Strategy Lead", name: "Edima Essien" },
+    { role: "Secretary", name: "Iyanuoluwa Adegboyega" },
+    { role: "Head of Marketing and Communication", name: "Kalkidan Wubshet" },
   ];
 
   return (
